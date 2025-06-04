231d9396a9befe2884803133767d259b249c003c
Hazim <hazim_malik@hotmail.com>
2025-06-04 20:31:37 +0200

    chore: add CI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("solidity-coverage");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.30",
